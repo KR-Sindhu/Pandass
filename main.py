62148a2887f1f130a2cc80648043bccb67cd188d
KR-Sindhu <sindhu.kr@diggibyte.com>
2024-08-08 19:07:08 +0530

    commit3

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import pandas as pd
-df = pd.DataFrame({'A': [1]})
-print(df.head())
+df_new = pd.DataFrame({'name': ['sindhu','bindu','indhu']})
+print(df_new.head())
+
+from utils import add_column,remove_column,filter_rows,rename_columns,sum_column
+df = add_column(df_new, 'age', [33,44,55])
+df = add_column(df, 'gender', ['F','F',"F"])
+df = add_column(df, 'salary', [33000,4500,45600])
+print(df)
+
+df = remove_column(df, 'salary')
+print(df)
+
+df = rename_columns(df,{'name':'fullname'})
+print(df)
+
+df = sum_column(df,'age')
+print("sum of age:",df)
+
+condition = lambda row:row['age']>40
+df = filter_rows(df,condition)
+print(df)
+ 
--- utils.py
+++ utils.py
@@ -1,2 +1,25 @@
-git config --global user.email "sindhu.kr@diggibyte.com"
-git config --global user.name "KR-Sindhu"
+import pandas as pd
+
+def add_column(df: pd.DataFrame, column_name: str, data: list) -> pd.DataFrame:
+    df[column_name] = data
+    return df
+
+def remove_column(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    df = df.drop(columns=[column_name])
+    return df
+
+def filter_rows(df: pd.DataFrame, condition) -> pd.DataFrame:
+    filtered_df = df[df.apply(condition, axis=1)]
+    return filtered_df
+
+def rename_columns(df: pd.DataFrame, columns_name: str) -> pd.DataFrame:
+    df = df.rename(columns=columns_name)
+    return df
+
+def sum_column(df: pd.DataFrame, column_name) -> float:
+    total = df[column_name].sum()
+    return total
+
+
+
+
